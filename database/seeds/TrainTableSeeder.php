0685b7678997c01271d0aaf344699ec4a9d4580d
Difelice-e <difelice.emanuele@gmail.com>
2022-04-27 15:00:08 +0200

    popolato db

--- database/seeds/TrainTableSeeder.php
+++ database/seeds/TrainTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Train;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainTableSeeder extends Seeder
 {
@@ -9,8 +11,23 @@ class TrainTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 100; $i++) {
+
+            $train = new Train();
+
+            $train->company = $faker->company();
+            $train->departure_station = $faker->city();
+            $train->arrival_station = $faker->city();
+            $train->departure = $faker->dateTimeBetween('-1 week, +1 week');
+            $train->arrival = $faker->dateTimeBetween('-1 week, +1 week');
+            $train->code = $faker->bothify('??-######');
+            $train->carriages_number = $faker->numberBetween(3,30);
+            $train->in_time = $faker->boolean();
+            $train->cancelled = $faker->boolean();
+
+            $train->save();
+        }
     }
 }
